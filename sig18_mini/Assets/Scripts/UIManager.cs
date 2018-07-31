e154e3b09ff0aa159de630fde88fc78e6ea902f2
mrahman13 <muhamed9412@gmail.com>
2018-07-31 13:12:52 -0400

    colors not syncing

--- sig18_mini/Assets/Scripts/UIManager.cs
+++ sig18_mini/Assets/Scripts/UIManager.cs
@@ -1,11 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class UIManager : MonoBehaviour {
+public class UIManager : MonoBehaviour, IPunObservable {
+    void IPunObservable.OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.isWriting)
+        {
+            stream.SendNext(Color.red);
+        }
+        else
+        {
+            Color c = (Color)stream.ReceiveNext();
+        }
+        foreach (Button b in Canvas.FindObjectsOfType<Button>())
+        {
+            b.GetComponent<Image>().color = Color.white;
+        }
+        GetComponent<Image>().color = Color.red;
 
-	// Use this for initialization
-	void Start () {
+    }
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
