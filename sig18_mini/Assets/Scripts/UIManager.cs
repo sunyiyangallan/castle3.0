57a2834d2f83cddf19bdba9df64041e4ffc4248f
mrahman13 <muhamed9412@gmail.com>
2018-07-31 13:27:23 -0400

    trying to implement RPCs

--- sig18_mini/Assets/Scripts/UIManager.cs
+++ sig18_mini/Assets/Scripts/UIManager.cs
@@ -2,9 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using FRL.IO;
 
-public class UIManager : MonoBehaviour, IPunObservable {
-    void IPunObservable.OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+public class UIManager : MonoBehaviour, IPunObservable, IPointerTriggerPressDownHandler, IPointerTriggerPressUpHandler
+{
+
+    public void OnPointerTriggerPressDown(XREventData eventData)
+    {
+        ColorRed();
+    }
+        void IPunObservable.OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
     {
         if (stream.isWriting)
         {
@@ -22,6 +29,15 @@ public class UIManager : MonoBehaviour, IPunObservable {
 
     }
 
+    [PunRPC]
+    public void ColorRed()
+    {
+        foreach (Button b in Canvas.FindObjectsOfType<Button>())
+        {
+            b.GetComponent<Image>().color = Color.white;
+        }
+        GetComponent<Image>().color = Color.red;
+    }
     // Use this for initialization
     void Start () {
 		
@@ -31,4 +47,14 @@ public class UIManager : MonoBehaviour, IPunObservable {
 	void Update () {
 		
 	}
+
+    void IPointerTriggerPressDownHandler.OnPointerTriggerPressDown(XREventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    void IPointerTriggerPressUpHandler.OnPointerTriggerPressUp(XREventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
 }
